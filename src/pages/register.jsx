40532df767fcf9e34892884fbba2cc5082d72092
Vincent <vincentiussss@gmail.com>
2019-12-03 22:38:53 +0700

    updated register req's,still need works

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -14,43 +14,57 @@ class registerPage extends Component {
         var numberRegex = /[0-9]/;
         var spcCharRegex = /[!@#$%^&*]/;
         
+        
         Axios.get(`http://localhost:2000/login?username=${fillUsername}&password=${fillPassword}`)
             .then((res) => {
                 if (res.data.length !== 0) {
                     return alert('ID sudah terambil');
                 }
                 if (fillPassword.length>=8){
-                    if(numberRegex.test(fillPassword)){
-                        if(spcCharRegex.test(fillPassword)){
-                            document.getElementById('spcTrue').innerHTML=`
-                            <p class="notifier-sukses">Password sudah memiliki sepsial karakter</p>
-                            `
-                            Axios.post('http://localhost:2000/login', {
-                                username: fillUsername,
-                                password: fillPassword,
-                                role: fillRole
-                            })
-                            .then((res) => {
-                                console.log(res);
-                                console.log(res.data)
-                                this.props.login(res.data)
-                                
-                            })
-                            }
-                            document.getElementById('numberTrue').innerHTML=`
-                            <p class="notifier-sukses">Password sudah memiliki angka</p>
-                            `
-                    }
                     return (
                         document.getElementById('karakterTrue').innerHTML=`
                         <p class="notifier-sukses">Password sudah lebih dari 8 karakter</p>
                         `
                     )
+                } 
+
+                if(numberRegex.test(fillPassword)){
+                    return (
+                        document.getElementById('numberTrue').innerHTML=`
+                        <p class="notifier-sukses">Password sudah memiliki angka</p>
+                        `
+                    )
                 }
 
+                if(spcCharRegex.test(fillPassword)){
+                    return (
+                        document.getElementById('spcTrue').innerHTML=`
+                        <p class="notifier-sukses">Password sudah memiliki special karakter</p>
+                        `
+                    )
+                }
                 
+                else if(fillPassword.length>=8 && numberRegex.test(fillPassword) && spcCharRegex.test(fillPassword)) {
+                    Axios.post('http://localhost:2000/login', {
+                        username: fillUsername,
+                        password: fillPassword,
+                        role: fillRole
+                    })
+                    .then((res) => {
+                        console.log(res);
+                        console.log(res.data)
+                        this.props.login(res.data)
+                        
+                    })
+                }
                 else {
-                    
+                    return (
+                        document.getElementById('divNotifier').innerHTML=`
+                        <p id='karakterTrue' class="notifier-gagal">Password harus lebih dari 8 karakter</p>
+                        <p id='numberTrue' class="notifier-gagal">Password harus memiliki angka</p>
+                        <p id='spcTrue' class="notifier-gagal">Password harus memiliki spesial karakter</p>
+                        `
+                    )
                 }
             })
             .catch((err) => {
@@ -76,9 +90,11 @@ class registerPage extends Component {
                     <Input type='password' innerRef={(pass) => this.pass = pass} placeholder="Password"></Input>
                     <br />
                     <Button onClick={this.register}> Sign Up </Button>
+                    <div id='divNotifier'>
                         <p id='karakterTrue' className="notifier-gagal">Password harus lebih dari 8 karakter</p>
                         <p id='numberTrue' className="notifier-gagal">Password harus memiliki angka</p>
-                        <p id='spcTrue' className="notifier-gagal">Password harus memiliki karakter spesial</p>
+                        <p id='spcTrue' className="notifier-gagal">Password harus memiliki spesial karakter</p>
+                    </div>
                 </div>
             </div>
         )
